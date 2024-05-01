69520c8f174c4bb4c3e19406e5b69971bdfce773
TwistRU <pavlyuk.rs@dvfu.ru>
2024-05-02 09:39:45 +1000

    under dev tablo

---
+++ src/components/TabloPage/InProgressRaw.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const props = defineProps(['talon'])
+</script>
+<template>
+  <div class="cell">{{ $props.talon.name }}</div>
+  <div class="cell">{{ $props.talon. }}</div>
+</template>
+<style></style>
