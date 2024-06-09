4c5cfac26749183cf249c7b799a66527cc6554d2
TwistRU <pavlyuk.rs@dvfu.ru>
2024-06-09 18:22:37 +1000

    fix buefy installation

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import './assets/main.css'
 import 'bulma/css/bulma.css'
 import '@ntohq/buefy-next/dist/buefy.css'
-import { createApp } from 'vue/dist/vue.esm-bundler'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
@@ -9,7 +8,7 @@ import Buefy from '@ntohq/buefy-next'
 import auth from '@/auth'
 import apolloClient from '@/apollo'
 import { DefaultApolloClient } from '@vue/apollo-composable'
-import { h, provide } from 'vue'
+import { createApp, h, provide } from 'vue'
 
 const app = createApp({
   setup() {
@@ -19,7 +18,7 @@ const app = createApp({
 })
 const pinia = createPinia()
 
-app.use(Buefy)
+app.use(Buefy, {})
 app.use(pinia)
 app.use(router)
 app.use(auth)
